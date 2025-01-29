3124882d986e287c09135bc40f8ca54bb3663b3d
Matthew Jeshua Martin <matthew-martin@programmer.net>
2025-01-28 21:44:40 -0500

    Foundational blocks for the sector gridd upgrade

---
+++ src/main/java/com/horrorcore/GridCell.java
@@ -0,0 +1,34 @@
+package com.horrorcore;
+
+public class GridCell {
+    private Digimon occupant;
+    private Building building;
+    private final int x;
+    private final int y;
+    private CellType type;
+
+    public void setType(CellType cellType) {
+        this.type = cellType;
+    }
+
+    public enum CellType {
+        NORMAL,
+        BORDER,  // Cells that connect to other sectors
+        BLOCKED  // Impassable terrain
+    }
+
+    public GridCell(int x, int y, CellType type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    // Getters/Setters
+    public Digimon getOccupant() { return occupant; }
+    public void setOccupant(Digimon occupant) { this.occupant = occupant; }
+    public Building getBuilding() { return building; }
+    public void setBuilding(Building building) { this.building = building; }
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public CellType getType() { return type; }
+}
